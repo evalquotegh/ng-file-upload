7085219521eb14e08a3437535237db6a4a1af7bd
evalquote <evalquote@icloud.com>
2022-09-12 16:14:27 +0200

    feat(3rd-party): setup file upload to cloudinary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,6 +13,7 @@ import { FileUploadComponent } from './file-upload/file-upload.component';
   declarations: [AppComponent, FileUploadComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
