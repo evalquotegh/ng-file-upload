2fb986333b2f4181afe753abe2d18bb2557c2647
evalquote <evalquote@icloud.com>
2022-09-12 22:45:06 +0200

    feat(service): add addImage method

--- src/app/image/image.service.ts
+++ src/app/image/image.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
 import { Image } from '../file-upload/file-upload.model';
 
 @Injectable({
@@ -12,5 +13,13 @@ export class ImageService {
   getImageList(): Observable<Image[]> {
     const url = 'http://localhost:3000/images';
     return this.httpClient.get<Image[]>(url);
+  }
+
+  addImage(image: Image): Observable<Image> {
+    const id = uuidv4(); // id is a json-server requirement for POST operation to work
+    const url = 'http://localhost:3000/images';
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const body = JSON.stringify({ id, ...image });
+    return this.httpClient.post<Image>(url, body, { headers });
   }
 }
